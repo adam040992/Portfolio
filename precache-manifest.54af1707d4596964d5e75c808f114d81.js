5ec58465da7b95d8e26bcfbb2348d1a6395fe1cc
Adam Walter <adam040992@gmail.com>
2020-09-15 18:58:31 +0200

    Updates

---
+++ precache-manifest.54af1707d4596964d5e75c808f114d81.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398b20a066fd0b677feee31078ed1631",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "59c77b05244fea8281a0",
+    "url": "/Portfolio/static/css/main.829ce082.chunk.css"
+  },
+  {
+    "revision": "126e4674abbf04ddfa99",
+    "url": "/Portfolio/static/js/2.f2deb8b6.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/Portfolio/static/js/2.f2deb8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c77b05244fea8281a0",
+    "url": "/Portfolio/static/js/main.0bc812ea.chunk.js"
+  },
+  {
+    "revision": "23ff405d518e183fcdd1",
+    "url": "/Portfolio/static/js/runtime-main.6c0d41e5.js"
+  },
+  {
+    "revision": "254f94dd743f45f6e07231ff49258765",
+    "url": "/Portfolio/static/media/1-min.254f94dd.jpg"
+  },
+  {
+    "revision": "70c6ce44e9169893d31e831cb54f819f",
+    "url": "/Portfolio/static/media/1-min.70c6ce44.png"
+  },
+  {
+    "revision": "86334467d2597738c4c839433938df8c",
+    "url": "/Portfolio/static/media/1-min.86334467.jpg"
+  },
+  {
+    "revision": "8820320cbc728fcf7e479b386d91e800",
+    "url": "/Portfolio/static/media/1-min.8820320c.jpg"
+  },
+  {
+    "revision": "fc2841a47d67683b8ca6f5ba548fd8bd",
+    "url": "/Portfolio/static/media/1-min.fc2841a4.png"
+  },
+  {
+    "revision": "4686c736b77ed430b5a75fd46d16ac10",
+    "url": "/Portfolio/static/media/10-min.4686c736.jpg"
+  },
+  {
+    "revision": "a60a37db72963dec8ee1890766c75b23",
+    "url": "/Portfolio/static/media/11-min.a60a37db.jpg"
+  },
+  {
+    "revision": "5e0862eae9ec0d07275e7323733ea5de",
+    "url": "/Portfolio/static/media/2-min.5e0862ea.jpg"
+  },
+  {
+    "revision": "e13b2afc38ddd80f61250de244c31bf8",
+    "url": "/Portfolio/static/media/2-min.e13b2afc.jpg"
+  },
+  {
+    "revision": "52ebfba457db9a6cd80b5b08d38eb0b7",
+    "url": "/Portfolio/static/media/2.52ebfba4.png"
+  },
+  {
+    "revision": "52caa82768fdc86627accf83bbe8fbbd",
+    "url": "/Portfolio/static/media/3-min.52caa827.jpg"
+  },
+  {
+    "revision": "c107efa570396ec9e6cdb88ce8e414e5",
+    "url": "/Portfolio/static/media/3-min.c107efa5.jpg"
+  },
+  {
+    "revision": "24b0194c93f0c37aad20f52a4be60070",
+    "url": "/Portfolio/static/media/3bb-min.24b0194c.png"
+  },
+  {
+    "revision": "2d1c6aed0a224d8b80d776a0a2c47ca1",
+    "url": "/Portfolio/static/media/4-min.2d1c6aed.jpg"
+  },
+  {
+    "revision": "b245fae6f1c59bb68bb3909a7191e5ce",
+    "url": "/Portfolio/static/media/4-min.b245fae6.jpg"
+  },
+  {
+    "revision": "e64fc8ff1816e19a2056e0c6914a47b0",
+    "url": "/Portfolio/static/media/4b-min.e64fc8ff.jpg"
+  },
+  {
+    "revision": "0bb8102c410451a79a6c2b17af7431f5",
+    "url": "/Portfolio/static/media/5-min.0bb8102c.jpg"
+  },
+  {
+    "revision": "84fdb70c2d3ddccd581f0eebf742d156",
+    "url": "/Portfolio/static/media/5-min.84fdb70c.jpg"
+  },
+  {
+    "revision": "12a691930eb4f144ac9aa4adca682d6e",
+    "url": "/Portfolio/static/media/5A-min.12a69193.jpg"
+  },
+  {
+    "revision": "11e4d869b35ed8b03dc8d1cdc59cd7cd",
+    "url": "/Portfolio/static/media/6-min.11e4d869.jpg"
+  },
+  {
+    "revision": "a40a801b19d8dae0c0e7e7e4d154b73f",
+    "url": "/Portfolio/static/media/6-min.a40a801b.jpg"
+  },
+  {
+    "revision": "878bd6d4cd7e5d1b901222a391d5f396",
+    "url": "/Portfolio/static/media/6A-min.878bd6d4.jpg"
+  },
+  {
+    "revision": "0abd8ec9ef42ffafc0d3d68147ee9bb1",
+    "url": "/Portfolio/static/media/7-min.0abd8ec9.jpg"
+  },
+  {
+    "revision": "a183d8d55b64d4d5141ad2597d0c2273",
+    "url": "/Portfolio/static/media/7a-min.a183d8d5.png"
+  },
+  {
+    "revision": "1fcabaea0c4f1fa6cd359f63ab070ec9",
+    "url": "/Portfolio/static/media/8-min.1fcabaea.jpg"
+  },
+  {
+    "revision": "70491472c8ccfc3552a283431e36e70b",
+    "url": "/Portfolio/static/media/8-min.70491472.jpg"
+  },
+  {
+    "revision": "84bfb72823c2d9e030211f0e32630a78",
+    "url": "/Portfolio/static/media/9-min.84bfb728.jpg"
+  },
+  {
+    "revision": "373dcd46349d027a65b5956658b40b77",
+    "url": "/Portfolio/static/media/KOLOR.373dcd46.png"
+  },
+  {
+    "revision": "ab8411b88526060c0c7a1f82674d7404",
+    "url": "/Portfolio/static/media/ap2.ab8411b8.png"
+  },
+  {
+    "revision": "974e115aa12dbae75d3c7fe786db482c",
+    "url": "/Portfolio/static/media/garderoba2-min.974e115a.png"
+  },
+  {
+    "revision": "738039fd2e8c263e0a97b03c3915fb89",
+    "url": "/Portfolio/static/media/garderoba3-min.738039fd.png"
+  },
+  {
+    "revision": "8ce7f74acb4f0264d181786dce9519c4",
+    "url": "/Portfolio/static/media/garderoba4-min.8ce7f74a.png"
+  },
+  {
+    "revision": "929b1b8e183ed9bf44c674d7737dad52",
+    "url": "/Portfolio/static/media/kurtyna1-min.929b1b8e.png"
+  },
+  {
+    "revision": "40c922550481934b70c8f47afa82d068",
+    "url": "/Portfolio/static/media/kurtyna2-min.40c92255.png"
+  },
+  {
+    "revision": "6bc6f2b4addc69a70d45061c1a7ea19f",
+    "url": "/Portfolio/static/media/kurtyna3-min.6bc6f2b4.png"
+  },
+  {
+    "revision": "afe4e554a1726bde17f5aa6ab7503b44",
+    "url": "/Portfolio/static/media/kurtyna4-min.afe4e554.png"
+  },
+  {
+    "revision": "93b9657f66f277a8d9e69e6b03ed7b97",
+    "url": "/Portfolio/static/media/kurtyna5-min.93b9657f.jpg"
+  },
+  {
+    "revision": "5c1dba6b71abcb7902a91713508aea93",
+    "url": "/Portfolio/static/media/kurtyna6-min.5c1dba6b.jpg"
+  },
+  {
+    "revision": "711c00d7befe3a8dc3b1868aed5d1c8f",
+    "url": "/Portfolio/static/media/main-site.711c00d7.jpg"
+  },
+  {
+    "revision": "e8b5ba74083388c85fb0bced502307a2",
+    "url": "/Portfolio/static/media/łazienkaB1.e8b5ba74.jpg"
+  },
+  {
+    "revision": "9ec6e63d407026370bb80fc550bae978",
+    "url": "/Portfolio/static/media/łazienkaZ1.9ec6e63d.jpg"
+  }
+]);
